296ca837da84083cc621cac39abd46a2517cd83c
Stas Zytkiewicz <stas.zytkiewicz@gmail.com>
2019-12-15 12:37:34 +0100

    Added some js stuff

---
+++ rspiWebServer-1/appWebserver.py
@@ -0,0 +1,44 @@
+import datetime
+import re
+
+from flask import Flask, render_template, request
+import sqlite3
+import json
+
+app = Flask(__name__)
+
+numSamples = 0
+
+def getDatetimeObject(iso_string):
+    timestamp = re.sub(r"[:]|([-](?!((\d{2}[:]\d{2})|(\d{4}))$))", '', iso_string)
+    return datetime.datetime.strptime(timestamp, "%Y%m%dT%H%M%S.%f%z")
+
+
+@app.route("/data.json")
+def data():
+    global numSamples
+    conn = sqlite3.connect('../dsmr50.sqlite')
+    curs = conn.cursor()
+    if numSamples == 0:
+        curs.execute("select COUNT(id) from  telegrams")
+        numSamples = curs.fetchall()[0][0]
+
+    curs.execute("SELECT `1-0:1.7.0`, `timestamp` FROM telegrams ORDER BY timestamp DESC LIMIT " + str(numSamples))
+    data = curs.fetchall()
+    #datalist = [(float(v.split('*')[0]), str(getDatetimeObject(ts).time())) for v, ts in data if '*' in v]
+    datalist = [(1, float(v.split('*')[0])) for v, ts in data if '*' in v]
+    print(datalist)
+    return json.dumps(datalist)
+
+
+@app.route("/graph")
+def graph():
+    return render_template('graph.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        threaded=True,
+        host='127.0.0.1'
+    )
