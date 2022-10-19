b92e6d912b5bf6b7456f6bd44ed6a2f77a4f0194
Stas Zytkiewicz <stas.zytkiewicz@gmail.com>
2022-10-19 13:03:18 +0200

    More refining and added a bash starter

--- P1Reader/P1_reader_dsmr50.py
+++ P1Reader/P1_reader_dsmr50.py
@@ -25,6 +25,22 @@ ser.port = "/dev/ttyUSB0"
 
 db_path = 'dsmr50.1.sqlite'
 
+# differences between old meter and the new one.
+# See email stedin 13-10-2022
+old_1 = 10479 - 1517
+old_2 = 13317 - 1508
+old_t1 = 1944 - 152
+old_t2 = 4337 - 388
+
+
+def stop(v):
+    try:
+        ser.close()
+    except Exception:
+        pass
+    sys.exit(v)
+
+
 def make_db(db_path):
     cmd1 = """
                 create table electric
@@ -58,7 +74,7 @@ def make_db(db_path):
         conn.commit()
     except Exception as e:
         print(f"Failed to create dbase: {e}")
-        sys.exit(4)
+        stop(4)
     finally:
         conn.close()
 
@@ -69,9 +85,9 @@ def read_com():
         ser.open()
     except Exception as e:
         print(f"Error opening COM: {e}")
-        sys.exit("Fout bij het openen van %s. Aaaaarch." % ser.name)
+        stop(1)
 
-    read_trys = 30  # we wait 2 seconds so this will be around 60 seconds
+    read_trys = 30  # we don't poll until the end of time
     while read_trys:
         # Read 1 line
         try:
@@ -81,7 +97,7 @@ def read_com():
             print("Error reading serial socket, continuing")
             if not read_trys:
                 print("No start of telegram found, read timeout, quitting")
-                sys.exit(1)
+                stop(1)
             else:
                 read_trys = - 1
                 time.sleep(10)
@@ -93,13 +109,12 @@ def read_com():
                 print("start of telegram found, continue")
                 read_trys = 0
             else:
-                print("No telegram start found, polling after 2 seconds")
+                print("No telegram start found, continuing polling")
                 if not read_trys:
                     print("No start of telegram found, read timeout, quitting")
-                    sys.exit(2)
+                    stop(2)
                 else:
                     read_trys = - 1
-
 
     # If we come here we have a start of a telegram
     p1_lines = []
@@ -112,9 +127,13 @@ def read_com():
         except Exception as e:
             print(e)
             # we don't try again as this error means trouble after the above read
-            sys.exit(3)
+            stop(3)
         else:
-            p1 = p1.decode()
+            try:
+                p1 = p1.decode('ascii')
+            except UnicodeError as e:
+                print(f"Failed to decode: {p1} => {e}")
+                stop(6)
 
         if p1[0].isdigit():
             print(f"read => {p1}")
@@ -123,6 +142,7 @@ def read_com():
         elif p1.startswith("!"):
             # end of a telegram
             print("end of telegram")
+            ser.close()
             break
 
     return p1_lines
@@ -132,53 +152,83 @@ def parse_telegram(telegram):
     for line in telegram:
         line = line.strip()
         if line[0].isdigit():
-            # print(p1)
-            key, val = line.split('(', 1)
+            print(f"raw line => {line}")
+            try:
+                key, val = line.split('(', 1)
+            except Exception as e:
+                print(f"failed to parse line: {e}")
+                stop(6)
+
             if key not in obis2dbcol.keys():
                 print(f"key {key} not found in obis, continuing")
                 continue
-
-            # # power faillure doesn't look like valid values
-            # if '1-0:99.97.0' in key:
-            #     # special case with possible powerfailures list, first 4 entries looks like crap
-            #     val = val.split(')(')[4:]
-            #     val[-1] = val[-1].strip(')')
 
             if '0-1:24.2.1' in key:
                 # special case for gas, key value is different
                 print(f"gas key, val: {key}, {val}")
                 val = val.split(')(')
-                val[1] = val[1].split('*')[0]
+                val[1] = val[1][:-4]
                 try:
                     val[1] = float(val[1])
                 except Exception as e:
                     print(f"Failed to floatify val: e")
-                    continue
+                    stop(6)
                 else:
                     val[0] = f"20{val[0][0:2]}-{val[0][2:4]}-{val[0][4:6]} {val[0][6:8]}:{val[0][8:10]}:{val[0][10:12]}"
             else:
                 val = val[:-1]  # loose last )
-                if '*kW' in val:
-                    val = val.split('*')[0]
+                if key in ('1-0:1.8.1', '1-0:1.8.2', '1-0:2.8.1', '1-0:2.8.2', '1-0:1.7.0', '1-0:2.7.0'):
+                    # these are Kw values
+                    val = val[:-4]
                     try:
                         val = float(val)
                     except Exception as e:
                         print(f"Failed to floatify val: e")
-                        continue
+                        stop(6)
+
+                if key == '1-0:1.7.0' or key == '1-0:2.7.0':
+                    # we convert into Watts iso KW for current usage
+                    val = val * 1000
+
+                # here we make some adjustments, the meter was a refurbished one with old values
+                if key == '1-0:1.8.1':
+                    val = val + old_1
+                elif key == '1-0:1.8.2':
+                    val = val + old_2
+                elif key == '1-0:2.8.1':
+                    val = val + old_t1
+                elif key == '1-0:2.8.2':
+                    val = val + old_t2
 
             if type(val) is str and val.endswith('S'):
                 # it's a timestamp
-                val = f"20{val[0:2]}-{val[2:4]}-{val[4:6]} {val[6:8]}:{val[8:10]}:{val[0][10:12]}"
+                val = f"20{val[0:2]}-{val[2:4]}-{val[4:6]} {val[6:8]}:{val[8:10]}:{val[10:12]}"
             obis[key] = val
 
     return obis
 
+def validate(db_dict):
+    #print(db_dict)
+
+    if not db_dict['created_at'] or '-' not in db_dict['created_at'] or ':' not in db_dict['created_at']:
+        return False
+    if not db_dict['delivered_1'] or not db_dict['delivered_2'] or not db_dict['returned_1'] or not db_dict['returned_2']:
+        return False
+    if  not db_dict['gas_delivered'] or '-' not in db_dict['gas_delivered'][0] or '-' not in db_dict['gas_delivered'][0]:
+        return False
+
+    return True
 
 def store(telegram):
     db_dict = {}
     for k, v in telegram.items():
         if k in obis2dbcol.keys():
             db_dict[obis2dbcol[k]] = v
+
+    if not validate(db_dict):
+        print("invalid values, skipping this one")
+        stop(5)
+
     gas_date, m3 = db_dict['gas_delivered']
     del db_dict['gas_delivered']
     conn = sqlite3.connect(db_path)
@@ -200,7 +250,7 @@ def main():
     telegram = read_com()
     if len(telegram) != 34:
         print(f"Incomplete telegram, is {len(telegram)} lines iso 34")
-        sys.exit(3)
+        stop(3)
 
     obis_dict = parse_telegram(telegram)
     # here we fetch the values we interested in from the telegram to put into our dbase
